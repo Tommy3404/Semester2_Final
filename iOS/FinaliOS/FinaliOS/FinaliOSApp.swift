2d0e627238fb370be587fd54773d9dd723cbabb2
Tommy McClure <tommy.mcclure@fhnw.edu>
2025-05-12 08:35:57 -0600

    base layout for website

---
+++ iOS/FinaliOS/FinaliOS/FinaliOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinaliOSApp.swift
+//  FinaliOS
+//
+//  Created by Tommy McClure on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FinaliOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
