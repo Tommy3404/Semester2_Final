ac4625d03979c5a88e6fc950388aade0a98debe6
Tommy McClure <tommy.mcclure@fhnw.edu>
2025-05-13 13:44:20 -0600

    added ethers

--- frontend/frontend/src/app/addEvent/page.tsx
+++ frontend/frontend/src/app/addEvent/page.tsx
@@ -1,11 +1,143 @@
 "use client"
 import Image from "next/image";
 import styles from "./page.module.css";
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import { ethers } from "ethers";
 
+
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+const abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "_walletAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "_physicalAddress",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "_walletAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "_physicalAddress",
+				"type": "string"
+			}
+		],
+		"name": "addAdmin",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "admins",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "walletAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "physicalAddress",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
 
 export default function Home() {
+
+  const [contract, setContract] = useState<ethers.Contract>()
+  const [event, setEvent] = useState("")
+
+  async function connectToWallet() {
+    if(contract !== undefined){
+      return;
+    }
+  
+
+  if((window as any).ethereum){
+    //We have metamask or another wallet
+
+    //Step 1 - Get the provider
+    let provider = new ethers.BrowserProvider((window as any).ethereum)
+
+    //Step 2 - Get the signer from the wallet
+    let signer = await provider.getSigner();
+
+    //Step 3 - Get our Contract instance with our address
+    //           abi, and our signer
+      setContract(new ethers.Contract(contractAddress, abi, signer))
+      console.log("Contract Created");
+      
+  }else{
+    alert("You need a wallet");
+  }
+  }
+
+
+async function addAdmin() {
+if(contract === undefined){
+  await connectToWallet();
+}
+
+if(contract){
+  // setAdmin(await contractAddress.addAdmin());
+  // setAddress(await contractAddress.addAddress());
+  contract.addAdmin("name", "walletAddress", "physicalAddress") //these are only place holders
+}
+
+}
+
+async function getEvent(){
+
+  let response = await fetch("http://localhost:3005/")
+  let data = await response.json();
+
+  console.log(data);
+  
+
+  setEvent(data.event);
+}
+
   return (
     <div className={styles.page}>
       <div className={styles.header}>
